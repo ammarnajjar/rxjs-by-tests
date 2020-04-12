9e3a76ea851e03227cc0bd86e59a114469e82f8d
Ammar Najjar <najjarammar@protonmail.com>
2020-04-12 23:07:08 +0200

    test: add parameterized test cases

--- src/observables.test.ts
+++ src/observables.test.ts
@@ -3,23 +3,31 @@ import { of, from, interval, timer, generate, range } from 'rxjs';
 import { take } from 'rxjs/operators';
 
 describe('Creators', () => {
-  const xArray = [0, 1, 2, 3];
-
-  test('from', () => {
-    const source = from(xArray);
-    const expected = cold('(abcd|)', { a: 0, b: 1, c: 2, d: 3 });
+  test.each([
+    [[0, 1, 2, 3], '(abcd|)', { a: 0, b: 1, c: 2, d: 3 }],
+    [[], '|', {}],
+  ])('from %p to %p => %p', (input, marblesSeq, marblesObj) => {
+    const source = from(input);
+    const expected = cold(marblesSeq, marblesObj);
     expect(source).toBeObservable(expected);
   });
 
-  test('of', () => {
-    const source = of(xArray);
-    const expected = cold('(a|)', { a: [0, 1, 2, 3] });
+  test.each([
+    [[0, 1, 2, 3], '(a|)', { a: [0, 1, 2, 3] }],
+    [[], '(a|)', { a: [] }],
+  ])('of %p to %p => %p', (input, marblesSeq, marblesObj) => {
+    const source = of(input);
+    const expected = cold(marblesSeq, marblesObj);
     expect(source).toBeObservable(expected);
   });
 
-  test('interval', () => {
-    const source = interval(10, getTestScheduler()).pipe(take(4));
-    const expected = cold('-abc(d|)', { a: 0, b: 1, c: 2, d: 3 });
+  test.each([
+    [10, '-abc(d|)', { a: 0, b: 1, c: 2, d: 3 }],
+    [0, '(abcd|)', { a: 0, b: 1, c: 2, d: 3 }],
+    [20, '--a-b-c-(d|)', { a: 0, b: 1, c: 2, d: 3 }],
+  ])('interval %p to %p => %p', (input, marblesSeq, marblesObj) => {
+    const source = interval(input, getTestScheduler()).pipe(take(4));
+    const expected = cold(marblesSeq, marblesObj);
     expect(source).toBeObservable(expected);
   });
 
@@ -39,9 +47,16 @@ describe('Creators', () => {
     expect(source).toBeObservable(expected);
   });
 
-  test('range', () => {
-    const source = range(0, 4);
-    const expected = cold('(abcd|)', { a: 0, b: 1, c: 2, d: 3 });
-    expect(source).toBeObservable(expected);
+  describe('range', () => {
+    test('with default step [=1]', () => {
+      const source = range(4);
+      const expected = cold('(abcd|)', { a: 0, b: 1, c: 2, d: 3 });
+      expect(source).toBeObservable(expected);
+    });
+    test('with custom step', () => {
+      const source = range(12, 4);
+      const expected = cold('(abcd|)', { a: 12, b: 13, c: 14, d: 15 });
+      expect(source).toBeObservable(expected);
+    });
   });
 });
