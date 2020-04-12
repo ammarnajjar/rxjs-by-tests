060e209742417f0c01e9b5a71099fe2151a9610e
Ammar Najjar <najjarammar@protonmail.com>
2020-04-12 03:41:36 +0200

    test: add range, generate, timer

--- src/observables.test.ts
+++ src/observables.test.ts
@@ -1,8 +1,8 @@
 import { cold, getTestScheduler } from 'jasmine-marbles';
-import { of, from, interval } from 'rxjs';
+import { of, from, interval, timer, generate, range } from 'rxjs';
 import { take } from 'rxjs/operators';
 
-describe('Observbles', () => {
+describe('Creators', () => {
   const xArray = [0, 1, 2, 3];
 
   test('from', () => {
@@ -20,6 +20,28 @@ describe('Observbles', () => {
   test('interval', () => {
     const source = interval(10, getTestScheduler()).pipe(take(4));
     const expected = cold('-abc(d|)', { a: 0, b: 1, c: 2, d: 3 });
+    expect(source).toBeObservable(expected);
+  });
+
+  test('timer', () => {
+    const source = timer(30, 10, getTestScheduler()).pipe(take(4));
+    const expected = cold('---abc(d|)', { a: 0, b: 1, c: 2, d: 3 });
+    expect(source).toBeObservable(expected);
+  });
+
+  test('generate', () => {
+    const source = generate(
+      0,
+      x => x <= 3,
+      x => x + 1,
+    );
+    const expected = cold('(abcd|)', { a: 0, b: 1, c: 2, d: 3 });
+    expect(source).toBeObservable(expected);
+  });
+
+  test('range', () => {
+    const source = range(0, 4);
+    const expected = cold('(abcd|)', { a: 0, b: 1, c: 2, d: 3 });
     expect(source).toBeObservable(expected);
   });
 });
